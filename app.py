f47a1f96e0dbc5d4a331d210eff648507fa929ab
wallacy <wallacy.ronan@gmail.com>
2020-11-12 18:53:18 -0300

    adicionando rota dinamicamente

--- app.py
+++ app.py
@@ -22,29 +22,51 @@ cb_destino.place(x=180, y=30)
 botao_1 = Button(app, width=8, text="Buscar", command="buscar", background="gray")
 botao_1.place(x=330, y=28)
 
-#criando e adicionando mapa na tela
-#imagem_1 = PhotoImage(file="img/america_sul.png")
-#mapa = Label(canvas, image = imagem_1)
-#mapa.place(x=10, y=70)
-
+#Componente para exibir o mapa e rota encontrada
 canvas = Canvas(width=480, height=515, bg="blue")
 canvas.place(x=10, y=70)
 
-#criando e adicionando mapa na tela
+#criando e adicionando mapa na canvas
 imagem_1 = PhotoImage(file="img/america_sul.png")
 mapa = Label(canvas, image = imagem_1)
 canvas.create_image(0, 0, image=imagem_1, anchor='nw')
 
-#Rota encontrada
+#Localização "geográfica" de cada cidade
+bogota = [121, 75]
+Quito = [83, 127]
+Lima = [100, 220]
+manaus = [353, 297]
+laPaz = [185, 220]
+brasilia = [265, 229]
+saoPaulo = [240, 127]
+Santiago = [134, 442]
+buenoAires = [240,410]
 
-#bogota la paz buenos Aires
-rota_1 = [121, 75, 185, 220, 240, 410]
+#rsequencia de cidades (será retornado a partir do main)
+caminho = [0, 4, 7]
 
-#manaus brasilia sao paulo
-rota_2 = [353, 297, 265, 229, 240, 127]
-#adicionando a rota na tela
+#transformando a caminho sequencia de pontos geometricos
+rota = []
+for i in caminho:
+    if i == 0:
+        rota += bogota
+    elif i== 1:
+        rota += Quito
+    elif i== 2:
+        rota += Lima
+    elif i== 3:
+        rota += manaus
+    elif i== 4:
+        rota += laPaz
+    elif i== 5:
+        rota += brasilia
+    elif i== 6:
+        rota += saoPaulo
+    elif i== 7:
+        rota += Santiago
+    else:
+        rota += buenoAires
 
-canvas.create_line(rota_2, fill="red", width=3)
-
+canvas.create_line(rota, fill="blue", width=3)
 
 app.mainloop()
